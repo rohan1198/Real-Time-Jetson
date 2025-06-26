5df191fa6077e62c3b3964fcbacccafb51745ad2
rohan1198 <rohan1198@gmail.com>
2025-06-26 18:44:44 +0100

    fixed gpu stress test

--- tests/stress_test.sh
+++ tests/stress_test.sh
@@ -25,7 +25,7 @@ BLUE=$(tput setaf 4)
 RESET=$(tput sgr0)
 
 
-if [ "$EUID" -ne 0 ]; then
+if [ "$EUID" -ne 0 ] || [ -z "$SUDO_USER" ]; then
   echo "${YELLOW}Please run this script with sudo.${RESET}"
   exit 1
 fi
@@ -62,14 +62,18 @@ echo "Stressing all cores ($ALL_CORES) and the GPU."
 taskset -c "$ALL_CORES" stress-ng $STRESS_NG_OPTS -t "$TEST_DURATION" &
 STRESS_PID=$!
 
-cd ~/gpu-burn
-./gpu_burn "$GPU_BURN_DURATION_SECS" &> /dev/null &
+SUDO_USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+GPU_BURN_PATH="${SUDO_USER_HOME}/gpu-burn"
+
+(cd "$GPU_BURN_PATH" && ./gpu_burn "$GPU_BURN_DURATION_SECS" &> /dev/null) &
 GPU_BURN_PID=$!
 
 cyclictest -D "$TEST_DURATION" -a "$MEASUREMENT_CORE" $CYCLICTEST_OPTS > "$LOG_DIR/3_full_system_load_test.txt"
 
 wait $STRESS_PID
-wait $GPU_BURN_PID
+if [ -n "$GPU_BURN_PID" ]; then
+    wait $GPU_BURN_PID
+fi
 echo "${GREEN}Test 3 complete.${RESET}"
 echo "---------------------------------------------------------"
 
